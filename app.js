81f5aa4c330b7d2e250192ffdae8bb80e41b098c
rudwk <tlsruwk12345@gmail.com>
2025-07-16 09:56:47 +0900

    port number(8080) -> process.env.port

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const WebSocket = require('ws');
 
 let clients = [];//앱, 웹, 단말기 등 소켓이 연결된 장치 배열
 
-const wss = new WebSocket.Server({ port: 8080 });//8080번 포트
-console.log(`WebSocket이 8080번 포트에에서 실행중`);
+const wss = new WebSocket.Server({ port: process.env.PORT });//8080번 포트
+console.log(`WebSocket이 8${process.env.PORT}080번 포트에에서 실행중`);
 
 wss.on('connection', (ws) => {
     console.log('연결됨.');
